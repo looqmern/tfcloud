34abc84f3d93a28f20b0ca9ccf67f74534bdda05
looqmern <87346835+looqmern@users.noreply.github.com>
2022-04-01 22:22:45 -0400

    modifiednewtf

--- main.tf
+++ main.tf
@@ -7,24 +7,22 @@
           source = "hashicorp/aws"
         }
       }
-      backend "remote" {
-#         # The name of your Terraform Cloud organization.
-        organization = "eluqs"
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-           name = "Github_com_lab1"
-        }
-      }
+    
+     cloud {
+    organization = "eluqs"
+
+    workspaces {
+      name = "Github_com_Lab1"
     }
-#
+  }
 
-# terraform {
-#   cloud {
-#     organization = "eluqs"
-
-#     workspaces {
-#       name = "Github_com_Lab1"
+   }
+#       backend "remote" {
+# #       organization = "eluqs"
+#          
+#         workspaces {
+#            name = "Github_com_lab1"
+#         }
+#       }
 #     }
-#   }
-
-#  }
+#
